aec214063ac01b4a1d96c3ae8b58a997ad598974
Henry <ag836@cornell.edu>
2024-04-03 14:06:03 -0400

    need to reposition, fix camera as well?

--- source/UIController.cpp
+++ source/UIController.cpp
@@ -21,6 +21,44 @@ bool UIController::init(std::shared_ptr<cugl::scene2::SceneNode> node, const std
     _shoottoggle = cugl::scene2::PolygonNode::allocWithTexture(assets->get<Texture>("shoottoggle"));
     _baittoggle = cugl::scene2::PolygonNode::allocWithTexture(assets->get<Texture>("baittoggle"));
     
+    // set the scale
+    _healthframe->setScale(UI_SCALE);
+    _healthfill->setScale(UI_SCALE);
+    _sizeframe->setScale(UI_SCALE);
+    _sizefill->setScale(UI_SCALE);
+    _bombtoggle->setScale(UI_SCALE);
+    _shoottoggle->setScale(UI_SCALE);
+    _baittoggle->setScale(UI_SCALE);
+    
+    // set the position
+    _healthframe->setAnchor(Vec2::ANCHOR_BOTTOM_LEFT);
+    _healthfill->setAnchor(Vec2::ANCHOR_BOTTOM_LEFT);
+    _sizeframe->setAnchor(Vec2::ANCHOR_BOTTOM_LEFT);
+    _sizefill->setAnchor(Vec2::ANCHOR_BOTTOM_LEFT);
+    _bombtoggle->setAnchor(Vec2::ANCHOR_BOTTOM_LEFT);
+    _shoottoggle->setAnchor(Vec2::ANCHOR_BOTTOM_LEFT);
+    _baittoggle->setAnchor(Vec2::ANCHOR_BOTTOM_LEFT);
+    
+//    float x = -screenSize.width/2;
+//    float y = -screenSize.height/2;
+//    
+//
+//    float healthx = x - _healthframe->getTexture()->getWidth();
+//    float healthy = y + screenSize.height - UI_SCALE * _healthframe->getTexture()->getHeight();
+//    
+//    _healthframe->setPosition(healthx, healthy);
+//    _healthfill->setPosition(healthx, healthy);
+//    
+//    
+//    _sizeframe->setPosition(x - _sizeframe->getTexture()->getWidth(), y - _sizeframe->getTexture()->getHeight());
+//    _sizefill->setPosition(x - _sizefill->getTexture()->getWidth(), y - _sizefill->getTexture()->getHeight());
+//    
+//    
+//    _bombtoggle->setPosition(x + screenSize.width - _bombtoggle->getTexture()->getWidth(), y + screenSize.height - _bombtoggle->getTexture()->getHeight());
+//    _shoottoggle->setPosition(x + screenSize.width - _shoottoggle->getTexture()->getWidth(), y + screenSize.height - _shoottoggle->getTexture()->getHeight());
+//    _baittoggle->setPosition(x + screenSize.width - _baittoggle->getTexture()->getWidth(), y + screenSize.height - _baittoggle->getTexture()->getHeight());
+    
+    
     node->addChild(_healthframe);
     node->addChild(_sizeframe);
     node->addChild(_bombtoggle);
@@ -28,6 +66,7 @@ bool UIController::init(std::shared_ptr<cugl::scene2::SceneNode> node, const std
     node->addChild(_baittoggle);
     node->addChild(_healthfill);
     node->addChild(_sizefill);
+    
     
     _dog = dog;
 
@@ -64,38 +103,3 @@ void UIController::setToggle(){
     _shoottoggle->setVisible(false);
     _baittoggle->setVisible(false);
 }
-
-//void UIController::draw(const std::shared_ptr<SpriteBatch>& batch, const Affine2& transform, Color4 tint)
-//{
-//    //Toggle
-//    cugl::Affine2 toggletrans;
-//    toggletrans.scale(UI_SCALE);
-//    toggletrans.translate(_screenSize.width - _bombtoggle->getWidth() * UI_SCALE, _screenSize.height - _bombtoggle->getHeight() * UI_SCALE);
-//   
-////    if(_dog->getMode() == "BOMB"){
-////        batch->draw(_bombtoggle, origin, toggletrans);
-////    } else if (_dog->getMode() == "SHOOT"){
-////        batch->draw(_shoottoggle, origin, toggletrans);
-////    } else if (_dog->getMode() == "BAIT"){
-////        batch->draw(_baittoggle, origin, toggletrans);
-////    }
-//    // Health bar
-//    cugl::Affine2 healthframetrans;
-//    healthframetrans.scale(UI_SCALE);
-//    healthframetrans.translate(0, _screenSize.height - _healthframe->getHeight() * UI_SCALE);
-//    
-//    cugl::Affine2 healthfilltrans;
-//    healthfilltrans.scale(UI_SCALE);
-//    healthfilltrans.translate(-1 * UI_SCALE, _screenSize.height - (_healthfill->getHeight() - 11) * UI_SCALE);
-//    
-////    batch->draw(getHealthBarTexture(_dog->getHealth()), origin, healthfilltrans);
-//    batch->draw(_healthframe, origin, healthframetrans);
-//    
-//    
-//    // Size bar
-//    cugl::Affine2 sizetrans;
-//    sizetrans.scale(UI_SCALE);
-//    sizetrans.translate(0, 0);
-//    batch->draw(_sizeframe, origin, sizetrans);
-////    batch->draw(getSizeBarTexture(_dog->getAbsorb()), origin, sizetrans);
-//}
