023f9924fa43b3d38639673bcbbdc0e18ab9ad5d
Colin Wu <45516888+CollinWoo@users.noreply.github.com>
2024-04-10 09:16:11 -0400

    add WorldSearchVertex code except for adding successors

--- source/WorldSearchVertex.cpp
+++ source/WorldSearchVertex.cpp
@@ -7,3 +7,104 @@
 
 #include "WorldSearchVertex.h"
 
+bool WorldSearchVertex::IsSameState( WorldSearchVertex &rhs )
+{
+
+    // same state in a maze search is simply when (x,y) are the same
+    if( (x == rhs.x) &&
+        (y == rhs.y) )
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+size_t WorldSearchVertex::Hash()
+{
+    size_t h1 = std::hash<float>{}(x);
+    size_t h2 = std::hash<float>{}(y);
+    return h1 ^ (h2 << 1);
+}
+
+float WorldSearchVertex::GoalDistanceEstimate( WorldSearchVertex &nodeGoal )
+{
+    return abs(x - nodeGoal.x) + abs(y - nodeGoal.y);
+}
+
+bool WorldSearchVertex::IsGoal( WorldSearchVertex &nodeGoal )
+{
+
+    if( (x == nodeGoal.x) &&
+        (y == nodeGoal.y) )
+    {
+        return true;
+    }
+
+    return false;
+}
+
+// This generates the successors to the given Node. It uses a helper function called
+// AddSuccessor to give the successors to the AStar class. The A* specific initialisation
+// is done for each node internally, so here you just set the state information that
+// is specific to the application
+bool WorldSearchVertex::GetSuccessors( AStarSearch<WorldSearchVertex> *astarsearch, WorldSearchVertex *parent_node )
+{
+
+    int parent_x = -1;
+    int parent_y = -1;
+
+    if( parent_node )
+    {
+        parent_x = parent_node->x;
+        parent_y = parent_node->y;
+    }
+    
+
+    WorldSearchVertex NewNode(0, 0, _world);
+
+    // push each possible move except allowing the search to go backwards
+
+    if( (GetMap( x-1, y ) < 9)
+        && !((parent_x == x-1) && (parent_y == y))
+      )
+    {
+        NewNode = WorldSearchVertex( x-1, y, _world);
+        astarsearch->AddSuccessor( NewNode );
+    }
+
+    if( (GetMap( x, y-1 ) < 9)
+        && !((parent_x == x) && (parent_y == y-1))
+      )
+    {
+        NewNode = WorldSearchVertex( x, y-1, _world);
+        astarsearch->AddSuccessor( NewNode );
+    }
+
+    if( (GetMap( x+1, y ) < 9)
+        && !((parent_x == x+1) && (parent_y == y))
+      )
+    {
+        NewNode = WorldSearchVertex( x+1, y, _world );
+        astarsearch->AddSuccessor( NewNode );
+    }
+
+        
+    if( (GetMap( x, y+1 ) < 9)
+        && !((parent_x == x) && (parent_y == y+1))
+        )
+    {
+        NewNode = WorldSearchVertex( x, y+1, _world);
+        astarsearch->AddSuccessor( NewNode );
+    }
+
+    return true;
+}
+
+float WorldSearchVertex::GetCost( WorldSearchVertex &successor )
+{
+    return 0;
+}
--- source/WorldSearchVertex.h
+++ source/WorldSearchVertex.h
@@ -10,22 +10,40 @@
 
 #include <stdio.h>
 #include "stlastar.h"
+#include "World.h"
 
 class WorldSearchVertex{
 private:
+    // the world this node is in
+    std::shared_ptr<World> _world;
 public:
     
+    // x coord of this node
     int x;
+    
+    // y coord of this node
     int y;
     
-    WorldSearchVertex() { x = y = 0; }
-    WorldSearchVertex( int px, int py ) { x=px; y=py; }
+    WorldSearchVertex( int px, int py,  std::shared_ptr<World> world) { x=px; y=py; _world = world }
     
+    ~WorldSearchVertex(){}
+    
+    // The heurstic used to determine the distance to the goal
     float GoalDistanceEstimate( WorldSearchVertex &nodeGoal );
+    
+    // Whether this node is the goal
     bool IsGoal( WorldSearchVertex &nodeGoal );
+    
+    // Adds the nodes you can move to from this node to the pathfinder
     bool GetSuccessors( AStarSearch<WorldSearchVertex> *astarsearch, WorldSearchVertex *parent_node );
+    
+    //The cost of moving to a successor
     float GetCost( WorldSearchVertex &successor );
+    
+    // Whether rhs is the current node
     bool IsSameState( WorldSearchVertex &rhs );
+    
+    // The hash of this node
     size_t Hash();
 };
 
