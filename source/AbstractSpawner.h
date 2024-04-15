2ed13c64b9bdc32c16a2bf25522a783a822664b6
AlanAnxinChen <99108296+AlanAnxinChen@users.noreply.github.com>
2024-04-15 13:27:11 -0400

    Revert "Implemented new spawner parsing and mechanics"

--- source/AbstractSpawner.h
+++ source/AbstractSpawner.h
@@ -14,49 +14,42 @@
 class AbstractSpawner{
 protected:
     std::shared_ptr<cugl::scene2::PolygonNode> polyNode;
-    float _regularDelay;
-    float _accumulatedDelay;
+    int _spawnRate;
+    int _respawnCnt;
     int _health;
-    float _delay;
-    float _timeElapsed;
+    int _delay;
+    double _timeElapsed;
     cugl::Vec2 _position;
     
 public:
-
+    
     std::shared_ptr<cugl::scene2::SceneNode> getSpawnerNode(){
         return polyNode;
     }
-    AbstractSpawner(float regularDelay, cugl::Vec2 pos, int health, float delay)
-    : _regularDelay(regularDelay)
+    AbstractSpawner(int rate, cugl::Vec2 pos, int health, int delay)
+    : _spawnRate(rate)
     , _position(pos)
     , _health(health)
     , _timeElapsed(0.0)
-    , _accumulatedDelay(-delay + regularDelay)
     , _delay{delay}
     {
     }
     
-    float getTimeElapsed() const{
+    double getTimeElapsed() const{
         return _timeElapsed;
     }
-
-    void updateTime(float dt){
-        _accumulatedDelay += dt;
-        _timeElapsed += dt;
-    }
-
     bool canSpawn() const {
-        return (_accumulatedDelay > _regularDelay);
+        return (_respawnCnt > _spawnRate);
     }
     void reloadSpawner() {
-        _accumulatedDelay = 0;
+        _respawnCnt = 0;
     }
     void setSceneNode(std::shared_ptr<cugl::Texture> _texture){
         polyNode = cugl::scene2::PolygonNode::allocWithTexture(_texture);
         polyNode->setContentSize(Vec2(1, 1));
     }
-    const float getAccumulatedDelay() const {
-        return _accumulatedDelay;
+    const int getCnt() const {
+        return _respawnCnt;
     }
     cugl::Vec2 getPos() const {
         return _position; }
