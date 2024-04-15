63ec825527806d81ec48a63e6b32f6c4af589ae3
AlanAnxinChen <99108296+AlanAnxinChen@users.noreply.github.com>
2024-04-15 11:38:37 -0400

    Implemented new spawner parsing and mechanics

--- source/MeleeSpawner.h
+++ source/MeleeSpawner.h
@@ -7,12 +7,18 @@
 
 #ifndef MeleeSpawner_hpp
 #define MeleeSpawner_hpp
+#include <cstdlib> 
 
 #include "AbstractSpawner.h"
 class MeleeSpawner: public AbstractSpawner{
+
+private:
+    std::string spawnType1;
+    std::string spawnType2;
+    std::string spawnType3;
     
 public:
-    MeleeSpawner(int rate, cugl::Vec2 pos, int health, int delay);
+    MeleeSpawner(int rate, cugl::Vec2 pos, int health, int delay, std::string spawnType1, std::string spawnType2, std::string spawnType3);
     virtual void update(MonsterController& monsterController, OverWorld& overWorld, float timestep) override;
 };
 
