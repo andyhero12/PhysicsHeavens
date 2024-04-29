ccb34c3f0bf53475531158dd57140aaea6965312
AlanAnxinChen <99108296+AlanAnxinChen@users.noreply.github.com>
2024-04-28 22:40:20 -0400

    Added screen shake for shoot and explode (#24)

--- source/NLGameScene.cpp
+++ source/NLGameScene.cpp
@@ -31,6 +31,7 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <algorithm>
 
 using namespace cugl;
 using namespace cugl::physics2::net;
@@ -90,6 +91,8 @@ using namespace cugl::physics2::net;
 
 #define FIXED_TIMESTEP_S 0.02f
 #define ROOT_NODE_SCALE 1
+
+#define SHAKING_DECAY 100.0f
 
 #pragma mark -
 #pragma mark Constructors
@@ -577,7 +580,7 @@ void GameScene::postUpdate(float dt)
     delta -= (computeActiveSize() / 2);
     Vec2 curr = -delta / _zoom;
     Vec2 pan = curr.lerp(previousPan, 0.9f);
-    _rootnode->applyPan(pan);
+    _rootnode->applyPan(pan + shakeMagnitude * Vec2(rand() / (float)RAND_MAX, rand() / (float)RAND_MAX));
     previousPan = pan;
     //
     //    std::vector<Vec2> upperPosHide;
@@ -611,6 +614,11 @@ void GameScene::postUpdate(float dt)
 
     _minimap->update();
     olddogPos = dogPos;
+
+    shakeMagnitude -= SHAKING_DECAY * dt;
+    if(shakeMagnitude < 0) {
+        shakeMagnitude = 0;
+    }
 }
 
 void GameScene::fixedUpdate()
@@ -634,6 +642,7 @@ void GameScene::fixedUpdate()
         }
         if (auto biteEvent = std::dynamic_pointer_cast<BiteEvent>(e))
         {
+            //shakeMagnitude = std::max(shakeMagnitude, 50.0f);
             overWorld.processBiteEvent(biteEvent);
         }
         if (auto recallEvent = std::dynamic_pointer_cast<RecallEvent>(e))
@@ -642,11 +651,13 @@ void GameScene::fixedUpdate()
         }
         if (auto explodeEvent = std::dynamic_pointer_cast<ExplodeEvent>(e))
         {
+            shakeMagnitude = std::max(shakeMagnitude, 40.0f);
             //            CULog("Explode Event Got");
             overWorld.processExplodeEvent(explodeEvent);
         }
         if (auto shootEvent = std::dynamic_pointer_cast<ShootEvent>(e))
         {
+            shakeMagnitude = std::max(shakeMagnitude, 40.0f);
             //            CULog("Explode Event Got");
             overWorld.processShootEvent(shootEvent);
         }
