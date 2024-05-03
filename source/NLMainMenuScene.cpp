8bd4e25af363329f6d4859f3e4d7dac6f3d6731e
Andyli9266 <lizekai926@gmail.com>
2024-05-02 20:39:22 -0400

    delete some commnent

--- source/NLMainMenuScene.cpp
+++ source/NLMainMenuScene.cpp
@@ -86,16 +86,6 @@ bool MainMenuScene::init(const std::shared_ptr<cugl::AssetManager>& assets) {
         }
     });
 
-    // _button1->setPosition(650,90);
-    // _button3->setPosition(600,0);
-    // _Logo->setPosition(-100,670);
-    // _button1->setPosition(650,90);
-    // _button3->setPosition(600,0);
-    // _Logo->setPosition(-100,670);
-    
-    // layer->setContentSize(dimen);
-    // layer->setContentSize(dimen);
-
     _counter = 1;
     switchFreq = 0.2;
     _isdown = Isdown::isNONE;
