ddaec3186c9d634771618e3b602d87729f2492ae
Andrew <hicheng530@gmail.com>
2024-05-16 22:29:01 -0400

    desync

--- source/AbstractEnemy.h
+++ source/AbstractEnemy.h
@@ -427,8 +427,8 @@ protected:
         }
         setVX(direction.normalize().x * goalSpeed);
         setVY(direction.normalize().y * goalSpeed);
-        setX(getX());
-        setY(getY());
+//        setX(getX());
+//        setY(getY());
         _prevDirection =_curDirection;
         _curDirection = AnimationSceneNode::convertRadiansToDirections(direction.getAngle());
     }
